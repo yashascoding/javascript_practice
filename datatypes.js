a8fd8a60fca2a094b5b8475f1907e481d186adac
yashas bhagwat <bhagwatyashas30@gmail.com>
2024-10-18 18:38:57 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+// alert("Hello")  we are using node js not browser so you will not get any alert 
+// console.log(3+3); console.log("yashas") Here code readablity is bakwaaas so dont write this 
+// some of the datatype
+// number => range is in between 2 to the power 53
+// bigint -used for big numbers
+// string " "
+// boolean 
+//  null - standalone value 
+// undefined - the variable for which any value is not assigned 
+ let age =24;
+ console.log(typeof age); //returns the type of age which is number 
+ console.log(typeof undefined); //this is undefined 
+ console.log(typeof null  ); //this returns object
