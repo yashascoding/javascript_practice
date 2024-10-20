91e3df49407dc3843f3ef6e085369848eca8b40f
yashas bhagwat <bhagwatyashas30@gmail.com>
2024-10-20 11:58:24 +0530

    Create conversion-operation.js

---
+++ conversion-operation.js
@@ -0,0 +1,50 @@
+// let num=33;
+// console.log(typeof num);
+// console.log(typeof (num));
+
+// let number="33ab";
+// console.log(typeof number); output string 
+// console.log(typeof (number)); outut string 
+
+// let convert=Number(number);
+// console.log(typeof convert) output number 
+
+// conversion works like this when it is converted into number type 
+// "33"=>33
+// "33ab"=>NaN (but the type is number only)
+// true=>1  false=>0
+
+// these are the modification when converted into boolean type 
+// 1=>true
+// 0=>false 
+// ""=>false 
+// "ayshas"=>true
+
+
+//************** operations ************ */
+
+// console.log(1+1);
+// console.log(2*1);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1="yashas";
+// let str2="bhagwat";
+// let str3=str1+str2;
+// console.log(str3);  output is yashasbhagwat
+
+// console.log(1+"2");  output 12
+// console.log(1+"2"+"3"); output 123
+// console.log(1+"2"+1); output 121
+// console.log(2+2+"2"); output 42
+// console.log(2*3/33) use bodmas   (But these are all not at all necessary in industry)
+
+// console.log(+true); return 1
+// console.log(+"") return 0
+
+// Summary of Key Differences of prefix and postfix:
+// Prefix (++x, --x): The value is updated first, and then the expression uses the new value.
+// Postfix (x++, x--): The value is used in the expression first, and then it is updated.
+
+
