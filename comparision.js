fa964ae478378077cce5a09f43d2169be3a96bf5
yashas bhagwat <bhagwatyashas30@gmail.com>
2024-10-20 12:23:06 +0530

    Create comparision.js

---
+++ comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);  these all are basic comparisions which returns true or false 
+
+// console.log("092">1); true
+// console.log("0">1);  false 
+
+// console.log(null>0); false 
+// console.log(null>=0); true 
+// console.log(null==0); false 
+// console.log(null<=0);  true 
+// the reason for this is equality check == and other comaparision operator like > < >= <= work differently  
+//comparision converts null into the number 0 thats why it is different 
+
+
+// console.log(undefined>=0) it returns false only whether it is > < >= or any comaparision operator 
+
+// These all are only for refernece they are not used in  industry so dont use this 
