ed4991c7268f9c641c0de3673df39c5a1bda6030
yashas bhagwat <bhagwatyashas30@gmail.com>
2024-10-21 19:47:20 +0530

    Create data type2.js

---
+++ data type2.js
@@ -0,0 +1,11 @@
+//primitive datatype 
+// string,null,boolean,undefined,symbol,bigint,number 
+
+// Reference non primitive 
+// Array string object 
+// the type of all these will be array 
+
+let c=["s","l"];
+console.log(typeof c)
+
+// The type of null is object normallly non primitive types are  of object only if we return the type of function then it is function only 
